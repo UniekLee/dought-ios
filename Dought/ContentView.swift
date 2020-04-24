d68eca6b647e7a5a8e64897e30713b3d14f1b378
Lee Watkins <me@unieklee.com>
2020-04-24 15:27:12 +0100

    Initial Commit

---
+++ Dought/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dought
+//
+//  Created by Lee Watkins on 24/04/2020.
+//  Copyright © 2020 Lee Watkins. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
