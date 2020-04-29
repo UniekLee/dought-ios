97903c290611c59f14c97b1d3a243e355a4cc8b3
Lee Watkins <me@unieklee.com>
2020-04-29 15:34:29 +0100

    Created a dev data for the Bake Step list

--- Dought/Bake Step/BakeStep.swift
+++ Dought/Bake Step/BakeStep.swift
@@ -15,3 +15,19 @@ struct BakeStep: Identifiable {
     let name: String
     let duration: Minutes
 }
+
+extension BakeStep {
+    static var devData: [BakeStep] = [
+        BakeStep(name: "Levain build", duration: 300),
+        BakeStep(name: "Autolyse", duration: 60),
+        BakeStep(name: "Mix", duration: 15),
+        BakeStep(name: "Bulk ferment", duration: 240),
+        BakeStep(name: "Pre shape", duration: 5),
+        BakeStep(name: "Rest", duration: 25),
+        BakeStep(name: "Shape", duration: 10),
+        BakeStep(name: "Rest", duration: 30),
+        BakeStep(name: "Retard", duration: 960),
+        BakeStep(name: "Bake covered", duration: 20),
+        BakeStep(name: "Bake uncovered", duration: 20),
+    ]
+}
