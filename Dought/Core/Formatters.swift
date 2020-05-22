2e6fdd3b643d34b4843a11bc78b141801606791a
Lee Watkins <me@unieklee.com>
2020-05-22 12:48:14 +0100

    Tidied up Date extensions

---
+++ Dought/Core/Formatters.swift
@@ -0,0 +1,32 @@
+//
+//  Formatters.swift
+//  Dought
+//
+//  Created by Lee Watkins on 22/05/2020.
+//  Copyright © 2020 Lee Watkins. All rights reserved.
+//
+
+import Foundation
+
+class Formatters {
+    enum FormatterError: Error {
+        case failed
+    }
+    
+    static let shared: Formatters = Formatters()
+    
+    lazy var plusHMinFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute]
+        formatter.unitsStyle = .abbreviated
+        formatter.zeroFormattingBehavior = .pad
+        return formatter
+    }()
+    
+    lazy var stepStartTimeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "HH:mm"
+        return formatter
+    }()
+}
--- Dought/Core/Foundation/Date_Extensions.swift
+++ Dought/Core/Foundation/Date_Extensions.swift
@@ -1,5 +1,5 @@
 //
-//  TimeCalculator.swift
+//  Date_Extensions.swift
 //  Dought
 //
 //  Created by Lee Watkins on 05/05/2020.
@@ -8,31 +8,6 @@
 
 import Foundation
 import Time
-
-typealias Minutes = Int
-
-class Formatters {
-    enum FormatterError: Error {
-        case failed
-    }
-    
-    static let shared: Formatters = Formatters()
-    
-    lazy var plusHMinFormatter: DateComponentsFormatter = {
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.hour, .minute]
-        formatter.unitsStyle = .abbreviated
-        formatter.zeroFormattingBehavior = .pad
-        return formatter
-    }()
-    
-    lazy var stepStartTimeFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "HH:mm"
-        return formatter
-    }()
-}
 
 extension Date {    
     private var timeless: Absolute<Day> {
@@ -55,21 +30,7 @@ extension Date {
         let period = Absolute<Day>(region: .current, date: self)
         return period.format(weekday: .fullName)
     }
-}
 
-extension Minutes {
-    typealias Seconds = Int
-    
-    var asTimeInterval: TimeInterval {
-        return TimeInterval(integerLiteral: Int64(self.asSeconds))
-    }
-    
-    var asSeconds: Seconds {
-        return self * 60
-    }
-}
-
-extension Date {
     static var today: Date {
         return Clock.system.today().firstInstant.date
     }
